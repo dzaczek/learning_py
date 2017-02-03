1d14133f647c9f197748bf697a5dd465cc4a34f6
dzaczek <sirjacek@gmail.com>
2017-02-03 07:03:44 +0100

    add SET for compare

--- duplicator.py
+++ duplicator.py
@@ -17,10 +17,13 @@ def remove_duplicates2(arg):
             list_sorted.append(i)
     return list_sorted
 
+def remove_duplicates3(arg):
+    return list(set(arg))
 
 if __name__ == '__main__':
     import timeit
 
-    a=[4,5,5,3,2,3,2,1,2,3,2,3,1,2,3,4,2,1,2,3,4,5,2,1,23,4,21,3,3,2,1,2,3,4,5,6,4,5,6,4]*100
-    print(timeit.timeit("remove_duplicates(a)", setup="from __main__ import remove_duplicates,a"))
-    print(timeit.timeit("remove_duplicates2(a)", setup="from __main__ import remove_duplicates2,a"))
+    a=[4,5,6,2,3,1,2,7,8,2]*1000
+    print("My def:   ",timeit.timeit("remove_duplicates(a)", setup="from __main__ import remove_duplicates,a"))
+    print("if not in: ",timeit.timeit("remove_duplicates2(a)", setup="from __main__ import remove_duplicates2,a"))
+    print("Set:      ",timeit.timeit("remove_duplicates3(a)", setup="from __main__ import remove_duplicates3,a"))
